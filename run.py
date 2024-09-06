c0243ab5f075c37284357b1a6bf37b05a825208b
rebibabo <2841329980@qq.com>
2024-09-06 14:00:38 +0800

    fixed download bug

--- run.py
+++ run.py
@@ -1,21 +1,23 @@
-from trainer import Trainer
-from my_dataset import MyDataset
+import warnings
+warnings.filterwarnings('ignore')
 from dotenv import load_dotenv
 load_dotenv()
+from trainer import Trainer
+from my_dataset import MyDataset
 
 if __name__ == '__main__':
     model_name = 'microsoft/codebert-base'
     train_dataset = MyDataset('./dataset/train.jsonl')
     train_dataset.tokenize(model_name, 'code', do_lower_case=True)
-    train_dataloader = train_dataset.to_dataloader(16, shuffle=True)
+    train_dataloader = train_dataset.to_dataloader(8, shuffle=True)
 
     eval_dataset = MyDataset('./dataset/valid.jsonl')
     eval_dataset.tokenize(model_name, 'code', do_lower_case=True)
-    eval_dataloader = eval_dataset.to_dataloader(16, shuffle=False)
+    eval_dataloader = eval_dataset.to_dataloader(8, shuffle=False)
 
     test_dataset = MyDataset('./dataset/test.jsonl')
     test_dataset.tokenize(model_name, 'code', do_lower_case=True)
-    test_dataloader = test_dataset.to_dataloader(16, shuffle=False)
+    test_dataloader = test_dataset.to_dataloader(8, shuffle=False)
 
     trainer = Trainer(
         model_name=model_name,
